9f1e7ce01bf6651328839d85f5f1b96877066ca3
samikshaaghodke <samikshaaghodke@gmail.com>
2024-03-06 21:34:26 +0100

    Add project files.

---
+++ HRMS.UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HRMS.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
